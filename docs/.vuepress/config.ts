8c8ffe5543427eabdd56fdfcebc15407c8f54cc1
chenglong <457731724@qq.com>
2024-10-30 01:25:42 +0800

    init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: './',
   lang: 'zh-CN',
   title: 'chenglong',
   description: '',
